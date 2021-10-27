2c2279a190fddd42eb99512554ea771ecffb8c0e
jamescross813 <crossjames813@gmail.com>
2021-10-26 23:29:43 -0500

    fixed typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :events, through: :user_events
     has_many :user_events
-    has many :groups, through: :user_groups
+    has_many :groups, through: :user_groups
     has_many :user_groups
     has_many :availabilities, through: :user_availabilities
     has_many :user_availabilities
