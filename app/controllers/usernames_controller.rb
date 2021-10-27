a9d15402de89ce80eace41d4a67e41a479e3b3b3
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:01:02 -0500

    removed mistake table and resources

--- app/controllers/usernames_controller.rb
+++
@@ -1,2 +0,0 @@
-class UsernamesController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :date_infos
   resources :groups
   resources :users
-  resources :usernames
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
