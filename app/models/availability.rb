47f2d1cbf9029a6047dc894d80e2f1c2135970d4
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:58:12 -0500

    added availability relationships

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,2 +1,8 @@
 class Availability < ApplicationRecord
+    has_many :users, through: :user_availabilities
+    has_many :user_availabilities
+    has_many :events, through: :events_availabilities
+    has_many :events_availabilities
+    has_many :groups, through: :group_availabilities
+    has_many :group_availabilities
 end
