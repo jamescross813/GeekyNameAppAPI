1096763133736f4cc914e3f50d4a5c23005d0f5c
jamescross813 <crossjames813@gmail.com>
2021-10-26 23:25:36 -0500

    removed repetition

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :group
-  belongs_to :user
   has_many :users, through: :user_events
   has_many :user_events
   has_many :groups
--- db/migrate/20211027024950_create_events.rb
+++ db/migrate/20211027024950_create_events.rb
@@ -2,7 +2,6 @@ class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
       t.references :group, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
       t.string :event_name
       t.string :description
       t.string :est_time
