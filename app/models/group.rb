aa05590d49f2e4156eb5851032d368530ca5fb5c
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:56:17 -0500

    removed unnecessary relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,7 @@ class Group < ApplicationRecord
     has_many :user_groups
     has_many :availabilities, through: :group_availabilities
     has_many :group_availabilities
-    has_many :events, through: :group_events
-    has_many :group_events
+    has_many :events
     has_many :friends, through: :friend_groups
     has_many :friend_groups
 end
