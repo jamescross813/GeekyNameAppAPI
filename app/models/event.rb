0fb9c6065c727bc883616d909aaa08dce027b4c2
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:59:24 -0500

    updated event relationships

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
   has_many :user_events
   has_many :groups
+  has_many :availabilities, through: :event_availabilities
+  has_many :event_availabilities
 end
