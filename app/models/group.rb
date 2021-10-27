37562d9e4605f17685fbdddc69140374b8a12377
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:44:32 -0500

    relationships added to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,8 @@
 class Group < ApplicationRecord
+    has_many :users, through: :user_groups
+    has_many :user_groups
+    has_many :availabilities, through: :group_availabilities
+    has_many :group_availabilities
+    has_many :events, through: :group_events
+    hans_many :group_events
 end
