91333dc6c95ae45b2465b88d81b7da93fbb49efe
jamescross813 <crossjames813@gmail.com>
2021-10-26 22:39:52 -0500

    relationships added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :events, through: :user_events
+    has_many :user_events
+    has many :groups, through: :user_groups
+    has_many :user_groups
+    has_many :availabilities, through: user_availabilities
+    has_many :user_availabilities
+    has_many :friends
 end
