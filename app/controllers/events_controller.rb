b1531320545f047b82824e10b5c007288960a820
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:18:02 -0500

    set up show action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,4 +5,14 @@ class EventsController < ApplicationController
         render json: events,
             except: [:created_at, :updated_at]
     end
+
+    def show
+        event = Event.find_by(id: params[:id])
+        if event
+            render json: event,
+                except: [:created_at, :updated_at]
+        else
+            render json: {message: "No Event created"}
+        end
+    end
 end
