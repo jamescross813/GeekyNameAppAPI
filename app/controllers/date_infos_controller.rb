f0f1d07bb69c3cb3510280b1a7a01b5c91d0f85c
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:10:38 -0500

    index action for date_info

--- app/controllers/date_infos_controller.rb
+++ app/controllers/date_infos_controller.rb
@@ -9,5 +9,10 @@ class DateInfosController < ApplicationController
         end
     end
 
+    def index 
+        date_infos = DateInfo.all
+        render json: date_infos,
+            except: [:created_at, :updated_at]
+    end
 
 end
