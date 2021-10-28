087a93fb7f46c84775ef8b5592696ae12d8642a2
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:40:56 -0500

    updated event realtionships

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,8 @@ class Group < ApplicationRecord
     has_many :user_groups
     has_many :availabilities, through: :group_availabilities
     has_many :group_availabilities
-    has_many :events
+    has_many :groups, through: :group_events
+    has_many :group_events
     has_many :friends, through: :friend_groups
     has_many :friend_groups
 end
