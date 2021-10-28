661352c566a4e009960daa6f1f64b809aded3adf
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:39:51 -0500

    group event typo fix

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ApplicationRecord
   has_many :users, through: :user_events
   has_many :user_events
   has_many :groups, through: :group_events
-  has_many :grou_events
+  has_many :group_events
   has_many :availabilities, through: :event_availabilities
   has_many :event_availabilities
 end
