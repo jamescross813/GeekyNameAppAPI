562518a948ce3f9142b087e9ab86c150ca460cfe
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:36:42 -0500

    create action wip added

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,19 @@
 class FriendsController < ApplicationController
+    
+    def create
+        friend = Friend.new(friend_params)
+        if friend.save
+            render json: friend,
+                except: [:created_at, :updated_at]
+        else
+            render json: {message: "Could not create friend"}
+        end
+    end
+
+    private
+
+    def friend_params
+        params.require(:friend).permit()
+        # need to add in way to get user_id and friends user_id
+    end
 end
