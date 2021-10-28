ec55211658cd073d16cacacf9d56d8c75365430c
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:20:39 -0500

    events table update

--- db/migrate/20211027024950_create_events.rb
+++ db/migrate/20211027024950_create_events.rb
@@ -1,10 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :events do |t|
-      t.references :group, null: false, foreign_key: true
       t.string :event_name
       t.string :description
       t.string :est_time
+      t.string :game_type
 
       t.timestamps
     end
