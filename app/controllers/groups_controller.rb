360bf03d7aaa6043c85cc0086848716121c14745
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:27:59 -0500

    added index action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,8 @@
 class GroupsController < ApplicationController
+    def index 
+        groups = Group.all
+        render json: groups,
+            except: [:created_at, :updated_at]
+    end
+    
 end
