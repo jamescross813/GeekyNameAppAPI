a360f3e2cbdea9f7e8d0d4dcb36ceffa8a0e179a
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:55:01 -0500

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
     has_many :availabilities, through: :user_availabilities
     has_many :user_availabilities
     has_many :friends
-    
+
     has_secure_password
+    validates :username, presence: true, uniqueness: true 
+    validates :password, presence: true
 end
