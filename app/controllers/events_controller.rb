0e44e19e2f95235fc4e1097f11ab74b91a9452e2
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:15:37 -0500

    added index action to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,8 @@
 class EventsController < ApplicationController
+
+    def index 
+        events = Event.all
+        render json: events,
+            except: [:created_at, :updated_at]
+    end
 end
