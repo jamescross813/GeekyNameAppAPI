a642c330aa3511a7f6365ff215aeda233f44241e
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:18:23 -0500

    formatting

--- app/controllers/date_infos_controller.rb
+++ app/controllers/date_infos_controller.rb
@@ -1,9 +1,10 @@
 class DateInfosController < ApplicationController
+
     def show
         date_info = DateInfo.find_by(id: params[:id])
         if date_info
-            render json:date_info,
-            except: [:created_at, :updated_at]
+            render json: date_info,
+                except: [:created_at, :updated_at]
         else
             render json: {message: "No such date"}
         end
