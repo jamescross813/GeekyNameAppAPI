3aa14475fcc01a4dcacb437a065eafb87991275f
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:47:56 -0500

    basic create action added to user controller, inc params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user,
+            except: [:create_at, :updated_at]
+        else
+            render json: {message: "Could not create user, try again"}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
