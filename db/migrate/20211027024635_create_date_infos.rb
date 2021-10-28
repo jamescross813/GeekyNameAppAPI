c7f72b52eb98af8fd00dff7a1efe59fceb3cad76
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:21:11 -0500

    date info table update

--- db/migrate/20211027024635_create_date_infos.rb
+++ db/migrate/20211027024635_create_date_infos.rb
@@ -3,6 +3,7 @@ class CreateDateInfos < ActiveRecord::Migration[6.1]
     create_table :date_infos do |t|
       t.integer :day
       t.string :month
+      t.integer :month_num
       t.integer :year
 
       t.timestamps
