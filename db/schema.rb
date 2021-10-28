47f5bda81c0ff6a405212d920d3619758e982c82
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:34:02 -0500

    seed data wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,119 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_10_27_034658) do
 
+  create_table "availabilities", force: :cascade do |t|
+    t.string "date"
+    t.string "time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "date_infos", force: :cascade do |t|
+    t.integer "day"
+    t.string "month"
+    t.integer "month_num"
+    t.integer "year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "event_availabilities", force: :cascade do |t|
+    t.integer "event_id", null: false
+    t.integer "availability_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["availability_id"], name: "index_event_availabilities_on_availability_id"
+    t.index ["event_id"], name: "index_event_availabilities_on_event_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "event_name"
+    t.string "description"
+    t.string "est_time"
+    t.string "game_type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "friend_groups", force: :cascade do |t|
+    t.integer "friend_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["friend_id"], name: "index_friend_groups_on_friend_id"
+    t.index ["group_id"], name: "index_friend_groups_on_group_id"
+  end
+
+  create_table "friends", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "friend_user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
+
+  create_table "group_availabilities", force: :cascade do |t|
+    t.integer "group_id", null: false
+    t.integer "availability_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["availability_id"], name: "index_group_availabilities_on_availability_id"
+    t.index ["group_id"], name: "index_group_availabilities_on_group_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "group_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_availabilities", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "availability_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["availability_id"], name: "index_user_availabilities_on_availability_id"
+    t.index ["user_id"], name: "index_user_availabilities_on_user_id"
+  end
+
+  create_table "user_events", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
+  create_table "user_groups", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "group_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_user_groups_on_group_id"
+    t.index ["user_id"], name: "index_user_groups_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "event_availabilities", "availabilities"
+  add_foreign_key "event_availabilities", "events"
+  add_foreign_key "friend_groups", "friends"
+  add_foreign_key "friend_groups", "groups"
+  add_foreign_key "friends", "users"
+  add_foreign_key "group_availabilities", "availabilities"
+  add_foreign_key "group_availabilities", "groups"
+  add_foreign_key "user_availabilities", "availabilities"
+  add_foreign_key "user_availabilities", "users"
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
+  add_foreign_key "user_groups", "groups"
+  add_foreign_key "user_groups", "users"
 end
