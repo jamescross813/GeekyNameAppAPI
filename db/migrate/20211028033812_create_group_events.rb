f52c8fe431ab5680e40abd6e63d47a05f2d6b342
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:41:34 -0500

    fixed table headings

--- db/migrate/20211028033812_create_group_events.rb
+++ db/migrate/20211028033812_create_group_events.rb
@@ -2,8 +2,7 @@ class CreateGroupEvents < ActiveRecord::Migration[6.1]
   def change
     create_table :group_events do |t|
       t.references :group, null: false, foreign_key: true
-      t.string :event
-      t.string :references
+      t.references :event, null: false, foreign_key: true
 
       t.timestamps
     end
