ce73c2a4942b506addeedd3028a975f1e38eca4f
jamescross813 <crossjames813@gmail.com>
2021-10-27 23:28:49 -0500

    show aciton added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,5 +4,15 @@ class GroupsController < ApplicationController
         render json: groups,
             except: [:created_at, :updated_at]
     end
-    
+
+    def show
+        group = Group.find_by(id: params[:id])
+        if group
+            render json: group,
+                except: [:created_at, :updated_at]
+        else
+            render json: {message: "No group found"}
+        end
+    end
+
 end
