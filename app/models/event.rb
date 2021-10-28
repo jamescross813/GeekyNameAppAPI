aa0850285865597a8fabededb4d984537fb38987
jamescross813 <crossjames813@gmail.com>
2021-10-27 22:39:30 -0500

    group event added as relationship

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event < ApplicationRecord
   belongs_to :group
   has_many :users, through: :user_events
   has_many :user_events
-  has_many :groups
+  has_many :groups, through: :group_events
+  has_many :grou_events
   has_many :availabilities, through: :event_availabilities
   has_many :event_availabilities
 end
