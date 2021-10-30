baf19a1bbab1be9f3561a4153d1f94a44fb2b1b7
jamescross813 <crossjames813@gmail.com>
2021-10-30 14:30:06 -0500

    index actiona dded

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,10 @@
 class FriendsController < ApplicationController
+
+    def index
+        friends = Friend.all
+        render json: friends,
+            except: [:created_at, :updated_at]
+    end
     
     def create
         friend = Friend.new(friend_params)
