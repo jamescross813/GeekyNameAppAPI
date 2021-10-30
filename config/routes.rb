9a7ae17c7b7dbda0dba671d9685007fdc4407996
jamescross813 <crossjames813@gmail.com>
2021-10-30 18:26:19 -0500

    possible route for log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  # get '/login' => 'sessions#new' 
+  # post '/login' => 'sessions#create' 
+  # post '/logout' => 'sessions#destroy'
+
   resources :group_events, only:[:show, :index]
   resources :friend_groups, only:[:show, :index]
   resources :event_availabilities, only:[:show, :index]
