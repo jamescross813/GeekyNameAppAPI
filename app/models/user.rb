1ae654915ea1927a982353b65e5e13a208ec0c55
jamescross813 <crossjames813@gmail.com>
2021-10-30 17:00:56 -0500

    commented out change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
     has_many :events, through: :user_events
     has_many :user_events
-    has_many :groups
-    # has_many :user_groups
+    has_many :groups, through: :user_groups
+    has_many :user_groups
+    # has_many :groups
     has_many :availabilities, through: :user_availabilities
     has_many :user_availabilities
     has_many :friends
