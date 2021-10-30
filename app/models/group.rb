4207b30da5d0f5508dc03a614e57fe5ecfb621d0
jamescross813 <crossjames813@gmail.com>
2021-10-30 16:57:42 -0500

    change to group user relationship

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,5 @@
 class Group < ApplicationRecord
-    has_many :users, through: :user_groups
-    has_many :user_groups
+    belongs_to :user
     has_many :availabilities, through: :group_availabilities
     has_many :group_availabilities
     has_many :events, through: :group_events
