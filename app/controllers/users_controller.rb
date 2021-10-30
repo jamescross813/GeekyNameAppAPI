c44f336ec112fa8947236bb1cc8383af86319bf5
jamescross813 <crossjames813@gmail.com>
2021-10-30 17:31:20 -0500

    added user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+
+    def show 
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user,
+                except: [:created_at, :updated_at],
+                include: [:user_events, :user_groups, :friends]
+        else
+            render json: {message: "No Event added"}
+        end
+    end
+
     def create
         user = User.new(user_params)
         if user.save
             render json: user,
             except: [:create_at, :updated_at]
+            include: [:user_events, :user_groups, :friends]
         else
             render json: {message: "Could not create user, try again"}
         end
