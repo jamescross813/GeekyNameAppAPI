6d894c3cdb27911cd4bd23f0c7e42a4de67e6ebb
jamescross813 <crossjames813@gmail.com>
2021-10-30 12:58:23 -0500

    update cors

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GekkyNameAppApi
+module Uncontentious
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -36,5 +36,15 @@ module GekkyNameAppApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
   end
 end
