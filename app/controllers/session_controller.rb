964bedd40346fd0a5389dd4880745f9ffc165efa
jamescross813 <crossjames813@gmail.com>
2021-10-30 18:57:24 -0500

    bindingpry added

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
     def create
+        binding.pry
         user = User.find_by(username: params[:username])
         if user && user.authenticate(params[:password])
             render json: user,
