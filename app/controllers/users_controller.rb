2f98d022a0ecad228245e31e103ee312205f0a8f
jamescross813 <crossjames813@gmail.com>
2021-10-31 01:19:09 -0500

    typo fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
         user = User.new(user_params)
         if user.save
             render json: user,
-            except: [:create_at, :updated_at]
+            except: [:create_at, :updated_at],
             include: [:user_events, :user_groups, :friends]
         else
             render json: {message: "Could not create user, try again"}
