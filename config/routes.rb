01e5c0254bf203fc4ca2265f1456507b47071350
jamescross813 <crossjames813@gmail.com>
2021-10-31 00:58:24 -0500

    attempt to set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # resources :session, only: [:create]
   post '/login', to: 'sessions#create' 
   post '/logout', to: 'sessions#destroy'
-
+  post '/signup', to: 'users#create'
 
   resources :group_events, only:[:show, :index]
   resources :friend_groups, only:[:show, :index]
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   resources :date_infos, only:[:show, :index]
   resources :groups
   resources :users
+
+ 
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
