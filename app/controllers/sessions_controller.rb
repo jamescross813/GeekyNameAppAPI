9186f9920d35ae917bdd32c740d075bbd7417b60
jamescross813 <crossjames813@gmail.com>
2021-10-30 19:14:36 -0500

    typo fixes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+    def create
+        # binding.pry
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            render json: user,
+            except: [:create_at, :updated_at],
+            include: [:user_events, :user_groups, :friends]
+        else
+            render json: {message: "Could not login, try again or signup"}
+        end
+    end
+  
+    def destroy
+      session.delete :username
+    end
+  
+end
